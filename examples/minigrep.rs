b85c162300ebd71ee767a179faad0c67f14e11d0
japan-da-man <suzukinao.nao.nao.nao@gmail.com>
2026-01-19 23:58:18 +0900

    feat: スマートポインタについて記載

--- examples/minigrep.rs
+++ examples/minigrep.rs
@@ -12,7 +12,7 @@ fn main() {
 
     let config = Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments {}", err);
-        process::exit(1)
+        process::exit(1);
     });
 
     if let Err(err) = rust_hello_cli::run(config) {
